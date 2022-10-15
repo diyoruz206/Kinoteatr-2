89cff8f2198672e6a568c83a7cbc537abc2d98bd
Diyorbek [FE14] <115080862+diyoruz206@users.noreply.github.com>
2022-10-15 17:59:29 +0500

    Update script.js

--- kino/js/script.js
+++ kino/js/script.js
@@ -2,25 +2,16 @@
 let numberOfFilms = +prompt('Qancha film ko`rgansiz?');
 let actorName = prompt('Bosh rolda qaysi aktyor qatnashgan ');
 let filmGenre = prompt('Qanaqa janrda suratga olingan ');
+let filmPrice = prompt('Filmga qancha pul sarflangan ');
 let personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: actorName,
     genres: filmGenre,
+    price: filmPrice,
     privat: false
 };
 console.log(personalMovieDB); 
-let numberOfFilms = +prompt('Qancha film ko`rgansiz?');
-let actorName = prompt('Bosh rolda qaysi aktyor qatnashgan ');
-let filmGenre = prompt('Qanaqa janrda suratga olingan ');
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: actorName,
-    genres: filmGenre,
-    privat: false
-};
-
  for (let i = 0; i < 4; i++){
     let a = prompt('Oxirgi ko`rgan filmlaringizdan biri?'),
           b = prompt('Uni qancha baholagan bo`lardingiz?');
