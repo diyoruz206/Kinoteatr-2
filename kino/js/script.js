1399a151c59d26e33a703077c83e4f4eb225c073
Diyorbek [FE14] <115080862+diyoruz206@users.noreply.github.com>
2022-10-15 17:45:18 +0500

    Update script.js

--- kino/js/script.js
+++ kino/js/script.js
@@ -10,13 +10,25 @@ let personalMovieDB = {
     privat: false
 };
 console.log(personalMovieDB); 
+let numberOfFilms = +prompt('Qancha film ko`rgansiz?');
+let actorName = prompt('Bosh rolda qaysi aktyor qatnashgan ');
+let filmGenre = prompt('Qanaqa janrda suratga olingan ');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: actorName,
+    genres: filmGenre,
+    privat: false
+};
 
- for (let i = 0; i < 2; i++){
-    let a = prompt('Oxirgi ko`rgan filmlaringizdan biri?', ''),
-          b = prompt('Uni qancha baholagan bo`lardingiz?', '');
+ for (let i = 0; i < 4; i++){
+    let a = prompt('Oxirgi ko`rgan filmlaringizdan biri?'),
+          b = prompt('Uni qancha baholagan bo`lardingiz?');
 
     if (a == null || b == null || a == '' || b == '') {
+        console.log('iltimos biror symbol kiriting')
     } else {
+        console.log('Xato')
         personalMovieDB.movies[a] = b
     }
 
